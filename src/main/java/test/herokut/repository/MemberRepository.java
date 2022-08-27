19ccd07e6c8e76602cf3b6d69001e9dffe099163
Min <rudals0326@gmail.com>
2022-08-27 23:19:17 +0900

    test

--- src/main/java/test/herokut/repository/MemberRepository.java
+++ src/main/java/test/herokut/repository/MemberRepository.java
@@ -1,8 +1,10 @@
 package test.herokut.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import test.herokut.domain.Member;
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
 
