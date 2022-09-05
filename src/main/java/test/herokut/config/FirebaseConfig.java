16cc3916c27b0a4314d5fe4a8e8b4bd93a55af81
Min <rudals0326@gmail.com>
2022-09-05 23:15:44 +0900

    test

--- src/main/java/test/herokut/config/FirebaseConfig.java
+++ src/main/java/test/herokut/config/FirebaseConfig.java
@@ -4,32 +4,45 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
+import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
 
-    @Bean
-    public FirebaseAuth firebaseAuth() throws IOException {
+    @Value("${firebase.sdk.path}")
+    private String firebaseSdkPath;
 
-        FileInputStream serviceAccount =
-                new FileInputStream("firebaseKey.json");
+    private FirebaseApp firebaseApp;
 
+    @PostConstruct
+    public FirebaseApp initializeFCM() throws IOException {
+        Resource resource = new ClassPathResource(firebaseSdkPath);
+        FileInputStream fis = new FileInputStream(resource.getFile());
         FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setCredentials(GoogleCredentials.fromStream(fis))
                 .build();
-
-        FirebaseApp.initializeApp(options);
-
-        return FirebaseAuth.getInstance(FirebaseApp.getInstance());
+        firebaseApp = FirebaseApp.initializeApp(options, "Hooky");
+        return firebaseApp;
     }
 
+    @Bean
+    public FirebaseAuth initFirebaseAuth() {
+        FirebaseAuth instance = FirebaseAuth.getInstance(firebaseApp);
+        return instance;
+    }
 
-
+    @Bean
+    public FirebaseMessaging initFirebaseMessaging() {
+        FirebaseMessaging instance = FirebaseMessaging.getInstance(firebaseApp);
+        return instance;
+    }
 }
