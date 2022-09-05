2751e9e092c571b974035ccc52ddad39d434397c
Min <rudals0326@gmail.com>
2022-09-05 22:37:49 +0900

    test

---
+++ src/main/java/test/herokut/config/FirebaseConfig.java
@@ -0,0 +1,34 @@
+package test.herokut.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Value("classpath:config/firebaseKey.json")
+    private Resource resource;
+
+    @Bean
+    public FirebaseAuth firebaseAuth() throws IOException {
+
+        FileInputStream serviceAccount =
+                new FileInputStream(resource.getFile());
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+        FirebaseApp.initializeApp(options);
+
+        return FirebaseAuth.getInstance(FirebaseApp.getInstance());
+    }
+}
